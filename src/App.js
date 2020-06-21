282c8bd9568ac5544a4a328cc456be3755d6240b
rhythm jayee <jayeerythm8@gmail.com>
2020-06-21 19:23:12 +0530

    back to scratch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import './App.css';
+
+import Login from "./components/LoginRegister/Login/Login"
+import SignUp from "./components/LoginRegister/Register/Register"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+            <li>
+              <Link to="/signup">signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+        <Route path="/" exact>
+          </Route>
+        <Route path="/login" exact>
+          {/* <Login/> */}
+          </Route>
+          <Route path="/signup" exact>
+           {/* <SignUp/> */}
+          </Route>
+          </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
